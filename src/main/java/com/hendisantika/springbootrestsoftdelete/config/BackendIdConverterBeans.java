10ef48f7def6dc450d2030c3fc1f5a5a83578da0
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-04 05:46:05 +0700

    #16 - Add permissionRoleIdConverter Bean in BackendIdConverterBeans

--- src/main/java/com/hendisantika/springbootrestsoftdelete/config/BackendIdConverterBeans.java
+++ src/main/java/com/hendisantika/springbootrestsoftdelete/config/BackendIdConverterBeans.java
@@ -1,6 +1,8 @@
 package com.hendisantika.springbootrestsoftdelete.config;
 
+import com.hendisantika.springbootrestsoftdelete.entity.PermissionRole;
 import com.hendisantika.springbootrestsoftdelete.entity.RoleUser;
+import com.hendisantika.springbootrestsoftdelete.entity.pk.PermissionRolePK;
 import com.hendisantika.springbootrestsoftdelete.entity.pk.RoleUserPK;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -48,5 +50,37 @@ public class BackendIdConverterBeans {
                 return new RoleUserPK(Long.parseLong(parts[0]), Long.parseLong(parts[1]));
             }
         };
+    }
+
+    @Bean
+    public BackendIdConverter permissionRoleIdConverter() {
+        return new BackendIdConverter() {
+
+            @Override
+            public boolean supports(Class<?> delimiter) {
+                return PermissionRole.class.equals(delimiter);
+            }
+
+            @Override
+            public String toRequestId(Serializable id, Class<?> entityType) {
+                PermissionRolePK pk = (PermissionRolePK) id;
+                return parseUrl(pk.getPermissionId(), pk.getRoleId());
+            }
+
+            @Override
+            public Serializable fromRequestId(String id, Class<?> entityType) {
+                if (id == null)
+                    return null;
+
+                String[] parts = id.split(DELIMITER);
+
+                return new PermissionRolePK(Long.parseLong(parts[0]), Long.parseLong(parts[1]));
+            }
+        };
+    }
+
+    // Parse the Spring Data Rest URL
+    private String parseUrl(Serializable id1, Serializable id2) {
+        return String.format("%s_%s", id1, id2);
     }
 }
