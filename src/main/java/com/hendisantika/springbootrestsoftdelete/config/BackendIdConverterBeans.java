f8b7d8c69d32ba879665935a8c5f2972b9109dad
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-04 05:37:00 +0700

    #6 - Add BackendIdConverterBeans

---
+++ src/main/java/com/hendisantika/springbootrestsoftdelete/config/BackendIdConverterBeans.java
@@ -0,0 +1,19 @@
+package com.hendisantika.springbootrestsoftdelete.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-rest-soft-delete
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 04/05/20
+ * Time: 05.36
+ */
+@Configuration
+public class BackendIdConverterBeans {
+
+    // The delimiter to be used in the URL
+    private static final String DELIMITER = "_";
+}
