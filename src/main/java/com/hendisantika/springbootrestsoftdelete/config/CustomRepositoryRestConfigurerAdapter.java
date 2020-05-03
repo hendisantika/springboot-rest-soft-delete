d1b85571fec86af88add0cf464c8fef3afc4307d
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-04 05:47:30 +0700

    #18 - Add configureRepositoryRestConfiguration in CustomRepositoryRestConfigurerAdapter

--- src/main/java/com/hendisantika/springbootrestsoftdelete/config/CustomRepositoryRestConfigurerAdapter.java
+++ src/main/java/com/hendisantika/springbootrestsoftdelete/config/CustomRepositoryRestConfigurerAdapter.java
@@ -1,7 +1,12 @@
 package com.hendisantika.springbootrestsoftdelete.config;
 
+import com.hendisantika.springbootrestsoftdelete.entity.PermissionRole;
+import com.hendisantika.springbootrestsoftdelete.entity.RoleUser;
+import com.hendisantika.springbootrestsoftdelete.entity.pk.PermissionRolePK;
+import com.hendisantika.springbootrestsoftdelete.entity.pk.RoleUserPK;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 
 import javax.persistence.EntityManagerFactory;
@@ -20,4 +25,30 @@ public class CustomRepositoryRestConfigurerAdapter extends RepositoryRestConfigu
 
     @Autowired
     private EntityManagerFactory entityManagerFactory;
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+
+        // PermissionRole Entity
+        config.withEntityLookup().forRepository(PermissionRoleRepository.class, (PermissionRole entity) -> {
+            return new PermissionRolePK(entity.getPermissionId(), entity.getRoleId());
+        }, PermissionRoleRepository::findOne);
+
+        // RoleUser Entity
+        config.withEntityLookup().forRepository(RoleUserRepository.class, (RoleUser entity) -> {
+            return new RoleUserPK(entity.getRoleId(), entity.getUserId());
+        }, RoleUserRepository::findOne);
+
+        List<Class<?>> entityClasses = getAllManagedEntityTypes(entityManagerFactory);
+
+        // Expose id's for all entity classes
+        for (Class<?> entityClass : entityClasses)
+            config.exposeIdsFor(entityClass);
+
+        // Return newly created entities in the response
+        config.setReturnBodyOnCreate(true);
+
+        // Return updated entities in the response
+        config.setReturnBodyOnUpdate(true);
+    }
 }
