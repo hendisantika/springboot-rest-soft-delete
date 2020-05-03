5f38c1dccbc9fa805193ca19c1b995047a1226d4
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-04 05:45:46 +0700

    #15 - Add roleUserIdConverter Bean in BackendIdConverterBeans

--- src/main/java/com/hendisantika/springbootrestsoftdelete/config/BackendIdConverterBeans.java
+++ src/main/java/com/hendisantika/springbootrestsoftdelete/config/BackendIdConverterBeans.java
@@ -1,6 +1,12 @@
 package com.hendisantika.springbootrestsoftdelete.config;
 
+import com.hendisantika.springbootrestsoftdelete.entity.RoleUser;
+import com.hendisantika.springbootrestsoftdelete.entity.pk.RoleUserPK;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.webmvc.spi.BackendIdConverter;
+
+import java.io.Serializable;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,4 +22,31 @@ public class BackendIdConverterBeans {
 
     // The delimiter to be used in the URL
     private static final String DELIMITER = "_";
+
+    @Bean
+    public BackendIdConverter roleUserIdConverter() {
+        return new BackendIdConverter() {
+
+            @Override
+            public boolean supports(Class<?> delimiter) {
+                return RoleUser.class.equals(delimiter);
+            }
+
+            @Override
+            public String toRequestId(Serializable id, Class<?> entityType) {
+                RoleUserPK pk = (RoleUserPK) id;
+                return parseUrl(pk.getRoleId(), pk.getUserId());
+            }
+
+            @Override
+            public Serializable fromRequestId(String id, Class<?> entityType) {
+                if (id == null)
+                    return null;
+
+                String[] parts = id.split(DELIMITER);
+
+                return new RoleUserPK(Long.parseLong(parts[0]), Long.parseLong(parts[1]));
+            }
+        };
+    }
 }
