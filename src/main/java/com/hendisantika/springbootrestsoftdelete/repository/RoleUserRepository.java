0e7d153965addf1f16da1a1898bf40404260c9d5
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-04 06:00:56 +0700

    #27 - Add PRoleUserRepository

---
+++ src/main/java/com/hendisantika/springbootrestsoftdelete/repository/RoleUserRepository.java
@@ -0,0 +1,20 @@
+package com.hendisantika.springbootrestsoftdelete.repository;
+
+import com.hendisantika.springbootrestsoftdelete.entity.RoleUser;
+import com.hendisantika.springbootrestsoftdelete.entity.pk.RoleUserPK;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-rest-soft-delete
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 04/05/20
+ * Time: 06.00
+ */
+@Repository
+@Transactional
+public interface RoleUserRepository extends SoftDeletesRepository<RoleUser, RoleUserPK> {
+}
