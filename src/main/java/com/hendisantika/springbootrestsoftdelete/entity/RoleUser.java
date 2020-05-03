56a5ad67bfd3b7fe021c4960ed551a022d9954c1
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-04 05:45:09 +0700

    #14 - Add RoleUserPK

--- src/main/java/com/hendisantika/springbootrestsoftdelete/entity/RoleUser.java
+++ src/main/java/com/hendisantika/springbootrestsoftdelete/entity/RoleUser.java
@@ -1,6 +1,7 @@
 package com.hendisantika.springbootrestsoftdelete.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.hendisantika.springbootrestsoftdelete.entity.pk.RoleUserPK;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
